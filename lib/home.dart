80ad56797cb328340b085690b9b0ac6580b6ff33
Gilmar Oliveira <gilmar.odp@gmail.com>
2024-03-16 16:18:15 -0300

    resolvendo lint

--- lib/home.dart
+++ lib/home.dart
@@ -8,8 +8,8 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  TextEditingController _controllerAlcool = TextEditingController();
-  TextEditingController _controllerGasolina = TextEditingController();
+  final TextEditingController _controllerAlcool = TextEditingController();
+  final TextEditingController _controllerGasolina = TextEditingController();
   String _textoResultado = '';
 
   void _calcular() {
@@ -47,61 +47,59 @@ class _HomeState extends State<Home> {
           ),
           backgroundColor: Colors.lightBlue,
         ),
-        body: Container(
-          child: SingleChildScrollView(
-            padding: EdgeInsets.all(32),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                Padding(
-                  padding: EdgeInsets.only(bottom: 32),
-                  child: Image.asset('images/logo.png'),
+        body: SingleChildScrollView(
+          padding: const EdgeInsets.all(32),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(bottom: 32),
+                child: Image.asset('images/logo.png'),
+              ),
+              const Padding(
+                padding: EdgeInsets.only(bottom: 10),
+                child: Text(
+                  'Saiba qual a melhor opção para abastecimento do seu carro',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 22.0,
+                  ),
+                  textAlign: TextAlign.justify,
                 ),
-                Padding(
-                  padding: EdgeInsets.only(bottom: 10),
-                  child: Text(
-                    'Saiba qual a melhor opção para abastecimento do seu carro',
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 22.0,
-                    ),
-                    textAlign: TextAlign.justify,
+              ),
+              TextField(
+                decoration: const InputDecoration(
+                  labelText: 'Preço Álcool, ex: 1.59',
+                ),
+                keyboardType: TextInputType.number,
+                controller: _controllerAlcool,
+              ),
+              TextField(
+                decoration: const InputDecoration(
+                  labelText: 'Preço Gasolina, ex: 3.15',
+                ),
+                keyboardType: TextInputType.number,
+                controller: _controllerGasolina,
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 10),
+                child: ElevatedButton(
+                  onPressed: () => _calcular(),
+                  child: const Text('Calcular'),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 10),
+                child: Text(
+                  _textoResultado,
+                  style: const TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20,
                   ),
                 ),
-                TextField(
-                  decoration: InputDecoration(
-                    labelText: 'Preço Álcool, ex: 1.59',
-                  ),
-                  keyboardType: TextInputType.number,
-                  controller: _controllerAlcool,
-                ),
-                TextField(
-                  decoration: InputDecoration(
-                    labelText: 'Preço Gasolina, ex: 3.15',
-                  ),
-                  keyboardType: TextInputType.number,
-                  controller: _controllerGasolina,
-                ),
-                Padding(
-                  padding: EdgeInsets.only(top: 10),
-                  child: ElevatedButton(
-                    onPressed: () => _calcular(),
-                    child: Text('Calcular'),
-                  ),
-                ),
-                Padding(
-                  padding: EdgeInsets.only(top: 10),
-                  child: Text(
-                    _textoResultado,
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 20,
-                    ),
-                  ),
-                ),
-              ]
-            ),
-          )
+              ),
+            ]
+          ),
         ),
       ),
     );
--- lib/main.dart
+++ lib/main.dart
@@ -2,5 +2,5 @@ import 'package:alcool_ou_gasolina/home.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  return runApp(Home());
+  return runApp(const Home());
 }
